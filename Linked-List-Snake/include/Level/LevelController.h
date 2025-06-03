afde25add242d3f0edf10c580d4b9460433892eb
ARAV1NDPC\Aravindan <aravindanputhalath@gmail.com>
2025-06-03 20:04:06 +0530

    Adding Level Model and Controller

--- Linked-List-Snake/include/Level/LevelController.h
+++ Linked-List-Snake/include/Level/LevelController.h
@@ -1,10 +1,23 @@
 #pragma once
+#include "../../include/Level/LevelModel.h"
 
 namespace Level {
+	//class LevelModel;
+	class LevelView;
 	class LevelController {
+	private:
+		LevelModel* level_model;
+		LevelView* level_view;
 	public:
+		LevelController();
+		~LevelController();
+
+
 		void initialize();
 		void update();
 		void render();
+
+		float getCellWidth();
+		float getCellHeight();
 	};
 }
--- Linked-List-Snake/include/Level/LevelModel.h
+++ Linked-List-Snake/include/Level/LevelModel.h
@@ -1,1 +1,23 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include "../../include/Level/LevelData.h"
+#include <vector>
+
+namespace Level {
+	class LevelModel {
+	private:
+		std::vector<LevelData> level_configuration;
+
+		float cell_width;
+		float cell_height;
+	public:
+		static const int number_of_rows = 28;
+		static const int number_of_colums = 50;
+
+		LevelModel();
+		~LevelModel();
+		void initialize(int width, int height);
+		float getCellWidth();
+		float getCellHeight();
+	};
+}
