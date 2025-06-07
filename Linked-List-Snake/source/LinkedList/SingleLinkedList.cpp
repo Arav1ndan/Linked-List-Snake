5a944987d3c608ff63bb7b404d9a7213eceabec3
ARAV1NDPC\Aravindan <aravindanputhalath@gmail.com>
2025-06-07 16:32:23 +0530

    Starting off with Linked List

---
+++ Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
@@ -0,0 +1,27 @@
+#include "LinkedList/SingleLinkedList.h"
+
+namespace LinkedList {
+	
+	SingleLinkedList::SingleLinkedList()
+	{
+		head_node = nullptr;
+	}
+	SingleLinkedList::~SingleLinkedList() = default;
+	
+	
+	void SingleLinkedList::initialize(float width, float height, sf::Vector2i position, Direction direction)
+	{
+		node_width = width;
+		node_height = height;
+		default_position = position;
+		default_direction = direction;
+	}
+	void SingleLinkedList::render()
+	{
+	}
+	Node* SingleLinkedList::createNode()
+	{
+		return new Node();
+	}
+}
+
