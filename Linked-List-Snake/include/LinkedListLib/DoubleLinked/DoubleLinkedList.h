a8003e2ec4f80410533131beb5c71f27b7c2afa2
ARAV1NDPC\Aravindan <aravindanputhalath@gmail.com>
2025-07-02 15:55:08 +0530

    Create Double Linked List

--- Linked-List-Snake/include/LinkedListLib/DoubleLinked/DoubleLinkedList.h
+++ Linked-List-Snake/include/LinkedListLib/DoubleLinked/DoubleLinkedList.h
@@ -1,1 +1,40 @@
 #pragma once
+#include "LinkedListLib/LinkedList.h"
+#include "LinkedListLib/Node.h"
+
+namespace LinkedListLib
+{
+	namespace DDoubleLinked
+	{
+		class DoubleLinkedList : public LinkedList
+		{
+		protected:
+			virtual Node* createNode() override;
+
+		public:
+			DoubleLinkedList();
+			~DoubleLinkedList();
+
+			void insertNodeAtTail() override;
+			void insertNodeAtHead() override;
+			void insertNodeAtMiddle() override;
+			void insertNodeAtIndex(int index, Node* new_node);
+
+			void shiftNodesAfterInsertion(Node* new_node, Node* cur_node, Node* prev_node);
+
+			void removeNodeAtTail() override;
+			void removeNodeAtHead() override;
+			void removeNodeAtMiddle() override;
+			void removeNodeAt(int index) override;
+			void removeNodeAtIndex(int index);
+			void removeAllNodes() override;
+			void removeHalfNodes() override;
+
+			void shiftNodesAfterRemoval(Node* cur_node);
+
+			Direction reverse() override;
+			void reverseNodeDirections();
+
+		};
+	}
+}
